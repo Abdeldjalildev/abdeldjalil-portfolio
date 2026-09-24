7674955bcf274ecbac25802dedaf393648d9aa64
KHALFA abdeldjalil <abdeldjalil.khalfa@enssmal.edu.dz>
2026-09-24 09:24:07 +0100

    feat(phase13): initialize configured App Check early

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { initializeConfiguredAppCheck } from './firebase/appCheck.ts'
+
+initializeConfiguredAppCheck()
 import App from './App.tsx'
 import { getFirebaseApp } from './firebase/app.ts'
 
